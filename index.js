5323c090fc1a3660b923ca2eedc11316dcb44225
Joel Edwards <joeledwards@gmail.com>
2016-12-08 15:48:25 -0700

    Re-worked the dialect with new ad updated fields.

--- index.js
+++ index.js
@@ -1,121 +1,175 @@
 const Joi = require('joi');
 
-// Directive schemas
-const Sequence = Joi.number().integer().required();
-const Channel = Joi.string().min(1).max(128).required();
-const ChannelList = Joi.array().items(Joi.alternatives().when('length', {
-  is: 0,
-  otherwise: Channel
-})).required();
-function Directive(directive) {
-  return Joi.string().valid(directive).required();
+// Actions
+const AnyAction = Joi.string().min(1).required();
+function Action(action) {
+  return Joi.string().valid(action).required();
 }
 
-// Response schemas
+// The client's sequence number
+const Sequence = Joi.number().integer().required();
+
+// Channels
+const Channel = Joi.string().min(1).max(128).required();
+const ChannelList = Joi.array().items(
+  Joi.alternatives().when('length', {
+    is: 0,
+    otherwise: Channel
+  })
+).required();
+
+// Status info
 const StatusMessage = Joi.string().min(1).required();
 const StatusDetails = Joi.string().allow('').optional();
 function StatusCode(code) {
   return Joi.number().integer().valid(code).required();
 }
 
-// Message schemas
+// Message info
 const Message = Joi.string().allow('').required();
 const Timestamp = Joi.date().iso().required();
 const UUID = Joi.string().uuid().required();
 
-module.exports = {
+// Schema generator function
+function Schema(schemaObject) {
+  return Joi.object().keys(schemaObject);
+}
+
+// Identifies the schema to use in order to validate the supplied object.
+function identifySchema(obj) {
+  if (obj) {
+    let category = DialectModule[obj.action];
+    let code = obj.code;
+
+    if (category) {
+      if (obj.action === 'msg') {
+        return category;
+      } else if (code) {
+        return category[code];
+      } else {
+        return category.request;
+      }
+    } else if (code) {
+      let category = DialectModule.general;
+
+      if (category) {
+        return category[code];
+      }
+    }
+  }
+
+  return undefined;
+}
+
+// The Joi validator function.
+function validate(object, validator, callback) {
+  if (typeof callback === 'function') {
+    return Joi.validate(object, validator, callback);
+  } else {
+    return Joi.validate(object, validator);
+  }
+}
+
+let DialectModule = {
   general: {
-    catchAllError: {
-      sequence: Sequence,
+    500: Schema({
+      seq: Sequence,
+      action: AnyAction,
       code: StatusCode(500),
       message: StatusMessage,
       details: StatusDetails
-    },
-    invalidFormatError: {
-      sequence: Sequence,
+    }),
+    400: Schema({
+      seq: Sequence,
+      action: AnyAction,
       code: StatusCode(400),
       message: StatusMessage,
       details: StatusDetails
-    }
+    })
   },
 
   subscribe: {
-    directive: {
-      sequence: Sequence,
-      directive: Directive('subscribe'),
+    request: Schema({
+      seq: Sequence,
+      action: Action('subscribe'),
       channel: Channel
-    },
-    success: {
-      sequence: Sequence,
+    }),
+    200: Schema({
+      seq: Sequence,
+      action: Action('subscribe'),
       code: StatusCode(200),
       channels: ChannelList
-    },
-    incorrectPermissionsError: {
-      sequence: Sequence,
+    }),
+    401: Schema({
+      seq: Sequence,
+      action: Action('subscribe'),
       code: StatusCode(401),
       message: StatusMessage,
       details: StatusDetails
-    }
+    })
   },
 
   unsubscribe: {
-    directive: {
-      sequence: Sequence,
-      directive: Directive('unsubscribe'),
+    request: Schema({
+      seq: Sequence,
+      action: Action('unsubscribe'),
       channel: Channel
-    },
-    success: {
-      sequence: Sequence,
+    }),
+    200: Schema({
+      seq: Sequence,
+      action: Action('unsubscribe'),
       code: StatusCode(200),
       channels: ChannelList
-    },
-    incorrectPermissionsError: {
-      sequence: Sequence,
+    }),
+    401: Schema({
+      seq: Sequence,
+      action: Action('unsubscribe'),
       code: StatusCode(401),
       message: StatusMessage,
       details: StatusDetails
-    },
-    notFoundError: {
-      sequence: Sequence,
+    }),
+    404: Schema({
+      seq: Sequence,
+      action: Action('unsubscribe'),
       code: StatusCode(404),
       message: StatusMessage,
       details: StatusDetails
-    }
+    })
   },
 
-  publish: {
-    directive: {
-      sequence: Sequence,
-      directive: Directive('publish'),
-      channel: Channel,
-      message: Message
-    },
-    incorrectPermissionsError: {
-      sequence: Sequence,
+  pub: {
+    request: Schema({
+      seq: Sequence,
+      action: Action('pub'),
+      chan: Channel,
+      msg: Message
+    }),
+    401: Schema({
+      seq: Sequence,
+      action: Action('pub'),
       code: StatusCode(401),
       message: StatusMessage,
       details: StatusDetails
-    },
-    noSubscriptionsError: {
-      sequence: Sequence,
+    }),
+    404: Schema({
+      seq: Sequence,
+      action: Action('pub'),
       code: StatusCode(404),
       message: StatusMessage,
       details: StatusDetails
-    }
+    })
   },
 
-  message: {
+  msg: Schema({
     id: UUID,
-    received: Timestamp,
-    channel: Channel,
-    message: Message
-  },
+    action: Action('msg'),
+    time: Timestamp,
+    chan: Channel,
+    msg: Message
+  }),
 
-  validate: function(object, validator, callback) {
-    if (typeof callback === 'function') {
-      return Joi.validate(object, validator, callback);
-    } else {
-      return Joi.validate(object, validator);
-    }
-  }
+  identifySchema,
+  validate,
 };
+
+module.exports = DialectModule;
